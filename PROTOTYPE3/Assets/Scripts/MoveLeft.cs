26e67d58cf1611b92895ab9319358d3a6c583650
AbstractThink81 <89855045+AbstractThink81@users.noreply.github.com>
2022-02-20 03:18:29 -0600

    prototype 3

---
+++ PROTOTYPE3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,35 @@
+﻿
+/*
+ * Ian Connors
+ * Prototype 3
+ * Moves the gameobject to the left at a fixed speed
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 30f;
+    public float leftBound = -15;
+    private PlayerController playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerControllerScript.gameOver)
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        //if the gameobject is an obstacle, destroy it
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+		{
+            Destroy(gameObject);
+		}
+    }
+}
---
+++ PROTOTYPE3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿
+/*
+ * Ian Connors
+ * Prototype 3
+ * Controls player jumping, death, and collisions
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpForce;
+    public ForceMode forceMode;
+    public float gravityModifier;
+    private Animator playerAnimator;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    private AudioSource playerAudio;
+    
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //set reference to rigidbody component
+        rb = GetComponent<Rigidbody>();
+        forceMode = ForceMode.Impulse;
+
+        playerAnimator = GetComponent<Animator>();
+        playerAnimator.SetFloat("Speed_f", 1.0f);
+
+        //audio source
+        playerAudio = GetComponent<AudioSource>();
+
+
+        //modify gravity
+        if (Physics.gravity.y > -10)
+		{
+            Physics.gravity = new Vector3(0, -27f, 0);
+		}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+		{
+            rb.AddForce(Vector3.up * jumpForce, forceMode);
+            isOnGround = false;
+
+            //set jump trigger in animator
+            playerAnimator.SetTrigger("Jump_trig");
+
+            //stop dirt particle
+            dirtParticle.Stop();
+
+            //play jump sound effect
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+		}
+    }
+	private void OnCollisionEnter(Collision collision)
+	{
+        if (collision.gameObject.CompareTag("Ground"))
+		{
+            isOnGround = true;
+
+            //start dirt particle again
+            dirtParticle.Play();
+		}
+            
+
+
+        else if (collision.gameObject.CompareTag("Obstacle"))
+		{
+            Debug.Log("Game Over");
+            gameOver = true;
+
+            //play death animation
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+
+            //play explosion particle effect
+            explosionParticle.Play();
+
+            //stop dirt particle
+            dirtParticle.Stop();
+
+            //play crash sound effect
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+	}
+}
