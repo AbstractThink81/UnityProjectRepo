13b6e1406151c94a6276742de93e4ba63d01c074
AbstractThink81 <89855045+AbstractThink81@users.noreply.github.com>
2022-03-06 17:33:56 -0600

    Assignment 5A finished

---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/ChangeOpacityOnCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ChangeOpacityOnCollision : MonoBehaviour
+{
+	public float opacityChanged;
+	public Tilemap tilemap;
+	//private Color color;
+	public void Start()
+	{
+		//color = tilemap.color;
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+			StartCoroutine(ChangeOpacity(opacityChanged));
+	}
+	IEnumerator ChangeOpacity(float newOpacity)
+	{
+		if (newOpacity < tilemap.color.a)
+		{
+			while (tilemap.color.a > newOpacity)
+			{
+				tilemap.color = new Vector4(tilemap.color.r, tilemap.color.g, tilemap.color.b, tilemap.color.a - 0.05f);
+				yield return new WaitForSeconds(0.01f);
+			}
+		}
+		else
+		{
+			while (tilemap.color.a < newOpacity)
+			{
+				tilemap.color = new Vector4(tilemap.color.r, tilemap.color.g, tilemap.color.b, tilemap.color.a + 0.05f);
+				yield return new WaitForSeconds(0.01f);
+			}
+		}
+		tilemap.color = new Vector4(tilemap.color.r, tilemap.color.g, tilemap.color.b, newOpacity);
+	}
+}
---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+    int maxScore = 5;
+
+    public AudioSource ding;
+
+    public GameObject greenGems;
+    public GameObject blueGems;
+
+    public bool win = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        greenGems.SetActive(false);
+        blueGems.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score < 27)
+            scoreText.text = "Gems: " + score + "/" + maxScore;
+        if (score == 5 && maxScore < 16)
+		{
+            maxScore = 16;
+            StartCoroutine(dingDing());
+            spawnGreen();
+		}
+        if (score == 16 && maxScore < 27)
+		{
+            maxScore = 27;
+            StartCoroutine(dingDing());
+            spawnBlue();
+		}
+        if (score == 27)
+		{
+            scoreText.text = "Gems: " + score + "/" + maxScore + "\nYOU WIN! Press R to retry!";
+            win = true;
+            if (Input.GetKeyDown(KeyCode.R))
+			{
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			}
+        }
+    }
+    void spawnGreen()
+	{
+        greenGems.SetActive(true);
+    }
+    void spawnBlue()
+    {
+        blueGems.SetActive(true);
+    }
+    IEnumerator dingDing()
+	{
+        for (int i = 0; i < 7; i++)
+		{
+            ding.Play();
+            yield return new WaitForSeconds(0.5f);
+		}
+        
+	}
+}
