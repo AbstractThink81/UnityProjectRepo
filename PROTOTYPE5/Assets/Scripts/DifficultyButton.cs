ff7c31117f3975a8b5efde0f706ed93061ff902d
AbstractThink81 <89855045+AbstractThink81@users.noreply.github.com>
2022-04-27 04:18:51 -0500

    Did prototype 5

---
+++ PROTOTYPE5/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Ian Connors
+ * Assignment 8 (Prototype 5)
+ * Sets up difficulty buttons
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+
+    private GameManager gameManager;
+
+    public int difficulty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(SetDifficulty);
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    void SetDifficulty()
+	{
+        gameManager.StartGame(difficulty);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
