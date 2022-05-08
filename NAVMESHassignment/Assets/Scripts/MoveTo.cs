27512c09383402e1a411da1148f32e9a972c9349
AbstractThink81 <89855045+AbstractThink81@users.noreply.github.com>
2022-05-07 21:30:23 -0500

    finished navmesh assignment

---
+++ NAVMESHassignment/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Ian Connors
+ * Pathfinding and AI
+ * Moves to a position set with a variable
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
